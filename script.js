f5b5107273a82b8bf04358672bedb0d5eb44feb9
turtle111-ops <mr.sparkyturtle@proton.me>
2025-02-02 19:51:17 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+
+
+function displayAllMatches() {
+fetch(`https://streamed.su/api/matches/all`)
+ .then(Response => Response.json())
+ .then(data => {
+    function displayMatches() {
+
+        const matchBox = document.getElementById('matchBox');
+        data.forEach(matches => {
+            const matchCard = document.createElement('div');
+            matchCard.classList.add('matchCard');
+            matchCard.textContent = matches.title;
+            const matchImage = document.createElement('img');
+            matchImage.src = `https://streamed.su/${matches.poster}`;
+            matchImage.setAttribute('data-fallback', `https://wallpaper.dog/large/20445057.jpg`);
+            matchImage.onerror = () => {
+            matchImage.src = matchImage.getAttribute('data-fallback');
+            matchImage.style.width = '10%';
+        };
+            matchCard.onclick = function() {
+            window.location.href = `server.html?id=${matches.id}`;
+        }
+            matchCard.appendChild(matchImage)
+            matchBox.appendChild(matchCard);
+        });
+    }
+    displayMatches();
+    console.log(data)});
+}
+
+
+
+displayAllMatches();
